cc6723916ea2ed6ff928c969bd25364d8c2c67c9
Aparks10 <aparks10uga@gmail.com>
2026-08-21 20:39:16 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -13,7 +13,7 @@
 //
 // Bump CACHE_NAME any time you want to force everyone's cached copy to be
 // thrown out and re-fetched (e.g. after a big index.html update).
-const CACHE_NAME = 'field-provisions-shell-v1';
+const CACHE_NAME = 'field-provisions-shell-v2';
 
 const SHELL_FILES = [
   './',
